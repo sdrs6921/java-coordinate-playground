e2a375619ef09423871e32b32447e84dbda6a6fc
sdrs6921 <sdrs6921@naver.com>
2021-11-17 23:01:34 +0900

    refactor : 직선 중복 체크 메서드 테스트 및 기능 구현

--- src/main/java/coordinate/domain/StraightLine.java
+++ src/main/java/coordinate/domain/StraightLine.java
@@ -2,25 +2,37 @@ package coordinate.domain;
 
 import coordinate.domain.vo.Point;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class StraightLine implements Line {
 
     private static final int NUMBER_OF_LINE_POINTS = 2;
 
     private static final String INVALID_NUMBER_OF_STRAIGHT_LINE_POINTS_EXCEPTION_MESSAGE = "선의 점의 개수는 2개여야 합니다";
+    private static final String DUPLICATE_LINE_POINTS_EXCEPTION_MESSAGE = "점 사이에 중복이 존재합니다";
 
     private final List<Point> points;
 
     public StraightLine(List<Point> points) {
         validateNumberOf(points);
+        validateDuplicateOf(points);
         this.points = points;
     }
 
     private void validateNumberOf(final List<Point> points) {
         if (points.size() != NUMBER_OF_LINE_POINTS) {
             throw new IllegalArgumentException(INVALID_NUMBER_OF_STRAIGHT_LINE_POINTS_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateDuplicateOf(final List<Point> points) {
+        Set<Point> distinctPoints = new HashSet<>(points);
+
+        if (distinctPoints.size() < points.size()) {
+            throw new IllegalArgumentException(DUPLICATE_LINE_POINTS_EXCEPTION_MESSAGE);
         }
     }
 
--- src/test/java/coordinate/domain/StraightLineTest.java
+++ src/test/java/coordinate/domain/StraightLineTest.java
@@ -4,10 +4,10 @@ import coordinate.domain.vo.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.offset;
@@ -18,7 +18,7 @@ class StraightLineTest {
     @DisplayName("선을 생성한다")
     void create() {
         //given
-        List<Point> points = Arrays.asList(new Point(0, 0), new Point(1, 1));
+        List<Point> points = asList(new Point(0, 0), new Point(1, 1));
 
         //when
         Line line = new StraightLine(points);
@@ -36,14 +36,25 @@ class StraightLineTest {
 
         //then
         assertThatIllegalArgumentException().isThrownBy(() -> new StraightLine(points))
-                .withMessageContaining("선의 점의 개수는 2개여야 합니다");
+                .withMessage("선의 점의 개수는 2개여야 합니다");
+    }
+
+    @Test
+    @DisplayName("점 사이에 중복이 존재하면 예외를 발생시킨다")
+    void create_throw_exception_with_duplicate_points() {
+        //given
+        List<Point> points = asList(new Point(0, 0), new Point(0, 0));
+
+        //then
+        assertThatIllegalArgumentException().isThrownBy(() -> new StraightLine(points))
+                .withMessage("점 사이에 중복이 존재합니다");
     }
 
     @Test
     @DisplayName("선의 길이를 반환한다")
     void length() {
         //given
-        List<Point> points = Arrays.asList(new Point(0, 0), new Point(1, 1));
+        List<Point> points = asList(new Point(0, 0), new Point(1, 1));
         Line line = new StraightLine(points);
 
         //when
